4b48c7d15923c88daca583f0d9b243ddbd8819ce
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-03-04 17:13:59 -0600

    Front - New User Screen

---
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/nuevo_usuario.kt
@@ -0,0 +1,11 @@
+package com.example.administradordeunidadesmviles_e2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class nuevo_usuario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nuevo_usuario)
+    }
+}
