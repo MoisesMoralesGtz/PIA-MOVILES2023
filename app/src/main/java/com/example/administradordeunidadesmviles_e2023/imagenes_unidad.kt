b7141eeb8c79a1c7240152018d74c9d93227a87f
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-03-05 15:44:10 -0600

    Front - Unit Images Screen

---
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/imagenes_unidad.kt
@@ -0,0 +1,11 @@
+package com.example.administradordeunidadesmviles_e2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class imagenes_unidad : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imagenes_unidad)
+    }
+}
