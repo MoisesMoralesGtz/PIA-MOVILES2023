6c8d5c4b133c01e18a675fa5d6dd42577044f6de
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-03-05 16:01:33 -0600

    Front - Unit Administrator Screen

---
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/administrar_unidades.kt
@@ -0,0 +1,11 @@
+package com.example.administradordeunidadesmviles_e2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class administrar_unidades : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_administrar_unidades)
+    }
+}
