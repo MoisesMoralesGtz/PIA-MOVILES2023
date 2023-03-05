0e5d77d06a81c273fb965e5d1c596aaa7a9c288a
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-03-05 16:20:42 -0600

    Front - Existing Accounts Screen

---
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/cuenta_existente.kt
@@ -0,0 +1,11 @@
+package com.example.administradordeunidadesmviles_e2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cuenta_existente : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cuenta_existente)
+    }
+}
---
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/solicitud_nuevas_cuentas.kt
@@ -0,0 +1,11 @@
+package com.example.administradordeunidadesmviles_e2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class solicitud_nuevas_cuentas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_solicitud_nuevas_cuentas)
+    }
+}
