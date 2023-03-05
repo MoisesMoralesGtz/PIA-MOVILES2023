7ba8a563c32f3ab5684ccf18df4933af18b28e27
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-03-05 15:31:33 -0600

    Front - Unit Advanced Details

---
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/detalles_avanzados.kt
@@ -0,0 +1,11 @@
+package com.example.administradordeunidadesmviles_e2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detalles_avanzados : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalles_avanzados)
+    }
+}
