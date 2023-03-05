86fea510dadaa1112f00fe15e342f7440f932649
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-03-05 16:44:37 -0600

    Front - Existing Unit Screen

---
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/unidad_existente.kt
@@ -0,0 +1,11 @@
+package com.example.administradordeunidadesmviles_e2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class unidad_existente : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_unidad_existente)
+    }
+}
