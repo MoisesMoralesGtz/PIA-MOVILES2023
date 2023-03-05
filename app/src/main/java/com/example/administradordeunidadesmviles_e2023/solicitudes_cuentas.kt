498a260b86710121e489c3284d545bc5d0987360
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-03-04 19:09:52 -0600

    Dropdown menu with some visual errors

---
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/solicitudes_cuentas.kt
@@ -0,0 +1,11 @@
+package com.example.administradordeunidadesmviles_e2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class solicitudes_cuentas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_solicitudes_cuentas)
+    }
+}
