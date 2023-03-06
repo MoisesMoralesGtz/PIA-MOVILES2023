47fc45ee195864397c436b4adfd5a321190556be
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-03-05 21:42:38 -0600

    News Screens Added, missing slider

---
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/inicio_turno_empleado.kt
@@ -0,0 +1,11 @@
+package com.example.administradordeunidadesmviles_e2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class inicio_turno_empleado : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_turno_empleado)
+    }
+}
---
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/main_empleado.kt
@@ -0,0 +1,11 @@
+package com.example.administradordeunidadesmviles_e2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class main_empleado : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_empleado)
+    }
+}
