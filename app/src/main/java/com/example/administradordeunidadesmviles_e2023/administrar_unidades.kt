4ced55327971d2837805fe7023dd872cdc5b5483
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-03-06 21:47:21 -0600

    Name Patch

--- app/src/main/java/com/example/administradordeunidadesmviles_e2023/administrar_unidades.kt
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/administrar_unidades.kt
@@ -12,6 +12,12 @@ class administrar_unidades : AppCompatActivity() {
         binding = ActivityAdministrarUnidadesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.textInputLayoutNCUENTA.setOnClickListener {
+            val intent = Intent(this, unidad_existente::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out)
+        }
+
         binding.btnEstadoU.setOnClickListener{
             val intent = Intent(this, nueva_unidad::class.java)
             startActivity(intent)
--- app/src/main/java/com/example/administradordeunidadesmviles_e2023/unidad_existente.kt
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/unidad_existente.kt
@@ -1,11 +1,28 @@
 package com.example.administradordeunidadesmviles_e2023
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.administradordeunidadesmviles_e2023.databinding.ActivityUnidadExistenteBinding
+
 
 class unidad_existente : AppCompatActivity() {
+    private lateinit var binding:ActivityUnidadExistenteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_unidad_existente)
+        binding = ActivityUnidadExistenteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnEliminarCuenta.setOnClickListener{
+            val intent = Intent(this, administrar_unidades::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out)
+        }
+
+        binding.btnVolver.setOnClickListener{
+            val intent = Intent(this, administrar_unidades::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.from_left_in, R.anim.from_right_out)
+        }
     }
 }
