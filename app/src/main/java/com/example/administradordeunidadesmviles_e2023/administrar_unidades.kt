3ce0a95474ecaacc11f258993d2c3f0fd429169c
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-03-06 21:41:47 -0600

    Act 0941

--- app/src/main/java/com/example/administradordeunidadesmviles_e2023/administrar_unidades.kt
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/administrar_unidades.kt
@@ -12,6 +12,12 @@ class administrar_unidades : AppCompatActivity() {
         binding = ActivityAdministrarUnidadesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnEstadoU.setOnClickListener{
+            val intent = Intent(this, nueva_unidad::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out)
+        }
+
         binding.btnCerrarSesion.setOnClickListener{
             val intent = Intent(this, main_administrador::class.java)
             startActivity(intent)
--- app/src/main/java/com/example/administradordeunidadesmviles_e2023/nueva_unidad.kt
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/nueva_unidad.kt
@@ -1,11 +1,28 @@
 package com.example.administradordeunidadesmviles_e2023
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.administradordeunidadesmviles_e2023.databinding.ActivityNuevaUnidadBinding
+
 
 class nueva_unidad : AppCompatActivity() {
+    private lateinit var binding:ActivityNuevaUnidadBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nueva_unidad)
+        binding = ActivityNuevaUnidadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnContinuar.setOnClickListener {
+            val intent = Intent(this, administrar_unidades::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out)
+        }
+
+        binding.btnVolver.setOnClickListener{
+            val intent = Intent(this, administrar_unidades::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.from_left_in, R.anim.from_right_out)
+        }
     }
 }
