25a6084dd763ec75857222c39f432e90e05915e3
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-04-16 20:23:12 -0600

    Actualizacion de Sliders, y logica de menus

--- app/src/main/java/com/example/administradordeunidadesmviles_e2023/inicio_turno_empleado.kt
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/inicio_turno_empleado.kt
@@ -1,13 +1,13 @@
 package com.example.administradordeunidadesmviles_e2023
 
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.inputmethod.InputMethodManager
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import com.example.administradordeunidadesmviles_e2023.databinding.ActivityInicioTurnoEmpleadoBinding
-import android.widget.TextView
+import android.widget.Toast
+import android.view.View
 
 class inicio_turno_empleado : AppCompatActivity() {
     private lateinit var binding:ActivityInicioTurnoEmpleadoBinding
@@ -16,20 +16,79 @@ class inicio_turno_empleado : AppCompatActivity() {
         binding = ActivityInicioTurnoEmpleadoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fun aparecerElementos(){
 
-        //Arreglos
-        val arrayOpciones=listOf("A","B","C","D","E","F")
+            binding.NeumaticosBox.visibility = View.VISIBLE
+            binding.lblNeumaticos.visibility = View.VISIBLE
 
-        val adapter = ArrayAdapter(this, R.layout.elemento_lista,arrayOpciones)
+            binding.LimpiezaBox.visibility = View.VISIBLE
+            binding.lblLimpieza.visibility = View.VISIBLE
 
-        binding.dropNeumatico.setAdapter(adapter)
+            binding.CarroceriaBox.visibility = View.VISIBLE
+            binding.lblCarroceria.visibility = View.VISIBLE
 
-        binding.dropNeumatico.setOnItemClickListener{adapter, view,i,l->
-            val vias = view as TextView
+            binding.lblGasolina.visibility = View.VISIBLE
+            binding.inputKilometraje.visibility = View.VISIBLE
+
+
         }
 
+        //Arreglos
+        val arrayOpciones=listOf("Perfecto","Bueno","Malo","Muy Malo")
 
+        //Adapter
+        val adapter = ArrayAdapter(this, R.layout.elemento_lista,arrayOpciones)
 
+        //Seteo de DropDownMenus
+
+        //Placas
+        var SelectPlacas=""
+        binding.dropPlacas.setAdapter(adapter)
+
+        binding.dropPlacas.onItemClickListener = AdapterView.OnItemClickListener {
+                adapterView, view, i, l ->
+
+            val SelectPlacas = adapterView.getItemAtPosition(i).toString()
+            Toast.makeText(this,SelectPlacas,Toast.LENGTH_SHORT).show()
+
+            //Linea para hacer que aparezcan todos los demas drops
+            aparecerElementos()
+        }
+        //Neumaticos
+        var EstadoNeumaticos=""
+        binding.dropNeumatico.setAdapter(adapter)
+
+        binding.dropNeumatico.onItemClickListener = AdapterView.OnItemClickListener {
+                adapterView, view, i, l ->
+
+            val EstadoNeumaticos = adapterView.getItemAtPosition(i).toString()
+            Toast.makeText(this,EstadoNeumaticos,Toast.LENGTH_SHORT).show()
+        }
+
+        //Carroceria
+        var EstadoCarroceria=""
+        binding.dropCarroceria.setAdapter(adapter)
+
+        binding.dropCarroceria.onItemClickListener = AdapterView.OnItemClickListener {
+                adapterView, view, i, l ->
+
+            val EstadoCarroceria = adapterView.getItemAtPosition(i).toString()
+            Toast.makeText(this,EstadoCarroceria,Toast.LENGTH_SHORT).show()
+        }
+
+        //Limpieza
+
+        var EstadoLimpieza=""
+        binding.dropLimpieza.setAdapter(adapter)
+
+        binding.dropLimpieza.onItemClickListener = AdapterView.OnItemClickListener {
+                adapterView, view, i, l ->
+
+            val EstadoLimpieza = adapterView.getItemAtPosition(i).toString()
+            Toast.makeText(this,EstadoLimpieza,Toast.LENGTH_SHORT).show()
+        }
+
+        //Movilidad
         binding.btnContinuar.setOnClickListener{
             val intent = Intent(this, captura_de_imagenes::class.java)
             startActivity(intent)
