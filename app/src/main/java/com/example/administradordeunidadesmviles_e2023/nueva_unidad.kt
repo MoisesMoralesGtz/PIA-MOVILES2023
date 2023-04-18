53c6a1b534471e6760e8ed0e8f0ed5f1329ca1ae
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-04-17 19:20:47 -0500

    Primeros avances del Dialog

--- app/src/main/java/com/example/administradordeunidadesmviles_e2023/nueva_unidad.kt
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/nueva_unidad.kt
@@ -1,6 +1,9 @@
 package com.example.administradordeunidadesmviles_e2023
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.AdapterView
@@ -30,15 +33,30 @@ class nueva_unidad : AppCompatActivity() {
 
         binding.dropCarroceriaNuevo.onItemClickListener = AdapterView.OnItemClickListener {
                 adapterView, view, i, l ->
-
             SelectPlacas = adapterView.getItemAtPosition(i).toString()
             Toast.makeText(this,SelectPlacas, Toast.LENGTH_SHORT).show()
         }
 
         binding.btnContinuar.setOnClickListener {
+
+            val dialogBinding=layoutInflater.inflate(R.layout.alertas_para_usuarios,null)
+            val myDialog = Dialog(this)
+
+            myDialog.setContentView(dialogBinding)
+
+            myDialog.setCancelable(false)
+            myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+            myDialog.show()
+
+            //Codigo de las opciones
+
+
+            /*
             val intent = Intent(this, administrar_unidades::class.java)
             startActivity(intent)
             overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out)
+            */
         }
 
         binding.btnVolver.setOnClickListener{
