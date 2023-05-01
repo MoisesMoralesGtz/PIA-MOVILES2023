897b2bae2b0d032ebd31193c998788aa0af9eb81
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-04-30 20:51:15 -0600

    Sistema de perfiles completado

--- app/src/main/java/com/example/administradordeunidadesmviles_e2023/MainActivity.kt
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.administradordeunidadesmviles_e2023
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.administradordeunidadesmviles_e2023.databinding.ActivityMainBinding
 import com.example.administradordeunidadesmviles_e2023.ValoresGlobales
@@ -19,11 +20,50 @@ class MainActivity : AppCompatActivity() {
 
     private val db = FirebaseFirestore.getInstance()
 
-    var password=""
-    var autorizado=false
-    var permisos=false
+    private fun verificacionUser(toString: String):Any {
+        val empleadosRf = db.collection("empleados")
+        empleadosRf.get()
+            .addOnSuccessListener { querySnapshot ->
+                for (document in querySnapshot) {
+                    if(document.id==binding.TxtUser.text.toString()){
 
+                        var password = document.data["password"] as String
+                        var autorizado =document.data["autorizado"] as Boolean
+                        var permisos = document.data["esAdministrador"] as Boolean
 
+                        Log.d("ADVERTENCIAS", password)
+
+                        if(password==binding.txtContra.text.toString()){
+                            //Contraseña y usuario coinciden
+
+                            if(autorizado){
+                                if(permisos){
+                                    val intent = Intent(this, main_administrador::class.java)
+                                    startActivity(intent)
+                                    overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out)
+                                }else{
+                                    val intent = Intent(this, main_empleado::class.java)
+                                    startActivity(intent)
+                                    overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out)
+                                }
+
+                            }else{
+                                //La cuenta aun no ha sido autorizada
+                                Toast.makeText(this,"La cuenta aún no ha sido autorizada, consulte a su superior.",Toast.LENGTH_SHORT).show()
+                            }
+
+                        }else{
+                            //Contraseña Incorrecta
+                            Toast.makeText(this,"La contraseña introducida es incorrecta, verifique nuevamente",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+                //ocupaflag
+                //Toast.makeText(this,"El usuario introducido no existe, consulte con su superior",Toast.LENGTH_SHORT).show()
+            }
+        return 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,55 +72,12 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnIngresar.setOnClickListener{
             //Validación del usuario
-
-            val usuarioIngresado = binding.TxtUser.text.toString()
-
-            var flagUser=false
-
-            val empleadosRf = db.collection("empleados")
-            empleadosRf.get()
-                .addOnSuccessListener { querySnapshot ->
-                    for (document in querySnapshot) {
-                        if(document.id == usuarioIngresado){
-                            flagUser=true
-                            password = document.data["password"] as String
-                            autorizado =document.data["autorizado"] as Boolean
-                            permisos = document.data["esAdministrador"] as Boolean
-
-                            break
-                        }
-                    }
-                }
-
-            if(flagUser){
-
-                if(password===binding.txtContra.text.toString()){
-                    //Contraseña y usuario coinciden
-                    if(autorizado){
-                        if(permisos){
-                            val intent = Intent(this, main_administrador::class.java)
-                            startActivity(intent)
-                            overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out)
-                        }else{
-                            val intent = Intent(this, main_empleado::class.java)
-                            startActivity(intent)
-                            overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out)
-                        }
-
-                    }else{
-                        //La cuenta aun no ha sido autorizada
-                        Toast.makeText(this,"La cuenta aún no ha sido autorizada, consulte a su superior.",Toast.LENGTH_SHORT).show()
-                    }
-
-                }else{
-                    //Contraseña Incorrecta
-                    Toast.makeText(this,"La contraseña introducida es incorrecta, verifique nuevamente",Toast.LENGTH_SHORT).show()
-                }
-
+            if(binding.TxtUser.text.toString() == "" || binding.txtContra.text.toString()==""){
+                Toast.makeText(this,"No puede dejar campos vacios, ingrese todos sus datos",Toast.LENGTH_SHORT).show();
             }else{
-                //Usuario Inexistente
-                Toast.makeText(this,"El usuario introducido no existe, consulte a su superior",Toast.LENGTH_SHORT).show()
+                verificacionUser(binding.TxtUser.text.toString())
             }
+
 
         }
 
