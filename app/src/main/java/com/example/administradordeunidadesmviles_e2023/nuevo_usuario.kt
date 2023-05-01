34fc20317eab9fbe27204697c32036f471521e55
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-04-30 22:46:14 -0600

    Validacion de autorizacion completada

--- app/src/main/java/com/example/administradordeunidadesmviles_e2023/nuevo_usuario.kt
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/nuevo_usuario.kt
@@ -1,8 +1,13 @@
 package com.example.administradordeunidadesmviles_e2023
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.example.administradordeunidadesmviles_e2023.databinding.ActivityNuevoUsuarioBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -32,10 +37,40 @@ class nuevo_usuario : AppCompatActivity() {
                     "esAdministrador" to false
                 )
             )
+            //Aqui es donde va el dialogo
 
-            val intent = Intent(this, main_empleado::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out)
+            //ALERTAS PARA USUARIO PT 2
+            val dialogBinding=layoutInflater.inflate(R.layout.solo_continuar,null)
+            val myDialog = Dialog(this)
+
+            myDialog.setContentView(dialogBinding)
+            myDialog.setCancelable(false)
+
+            //Texto del Dialog
+            val titulo = dialogBinding.findViewById<TextView>(R.id.tituloAlerta)
+            val texto = dialogBinding.findViewById<TextView>(R.id.textoAlerta)
+
+            titulo.text="Listo"
+            texto.text="Has completado el proceso para crear tu cuenta, comentale a tu superior, y espera a que autorizen tu cuenta."
+
+            //Dimensiones del Dialog
+            val width = (resources.displayMetrics.widthPixels * 0.90).toInt()
+            val height = (resources.displayMetrics.heightPixels * 0.40).toInt()
+
+            myDialog.window?.setLayout(width, height)
+            myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+            myDialog.show()
+
+            val Seguir = dialogBinding.findViewById<Button>(R.id.btnSeguirz)
+
+            Seguir.setOnClickListener{
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out)
+
+                Toast.makeText(this,"Solicitud realizada con éxito",Toast.LENGTH_SHORT).show()
+            }
         }
 
         binding.btnVolver.setOnClickListener{
