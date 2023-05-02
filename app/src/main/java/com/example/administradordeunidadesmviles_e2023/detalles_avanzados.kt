34cd288e7c73bb68afd3fc2cadb94f826c33276b
Michael Noguera <michael.noguerag@uanl.edu.mx>
2023-05-02 16:55:34 -0600

    Muestreo en vivo de estado terminado

--- app/src/main/java/com/example/administradordeunidadesmviles_e2023/detalles_avanzados.kt
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/detalles_avanzados.kt
@@ -3,14 +3,41 @@ package com.example.administradordeunidadesmviles_e2023
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.administradordeunidadesmviles_e2023.databinding.ActivityDetallesAvanzadosBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class detalles_avanzados : AppCompatActivity() {
     private lateinit var binding:ActivityDetallesAvanzadosBinding
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetallesAvanzadosBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Se leen los cambios del tipo de cuenta
+        val bundle = intent.extras
+        val unidadEspecificada = bundle?.getString("placas")
+        val unidadesRef = db.collection("automoviles").document(unidadEspecificada.toString())
+
+        unidadesRef.get()
+            .addOnSuccessListener { document ->
+                var datoLeido = document.data
+                binding.txtUnidad.text = document.id
+                var Dia = datoLeido?.get("fechaRegistro") as CharSequence?
+                var Hora = datoLeido?.get("horaRegistro") as CharSequence?
+                binding.txtFecha.text = "Fecha: "+Dia+" / "+Hora
+                binding.txtNeumaticos.text = datoLeido?.get("stateNeumaticos") as CharSequence?
+                binding.lblCarroceria.text = datoLeido?.get("stateCarroceria") as CharSequence?
+                binding.txtLimpieza.text = datoLeido?.get("stateLimpieza") as CharSequence?
+                binding.txtKilometraje.text = datoLeido?.get("stateKilometraje") as CharSequence?
+
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(this,"Placas no encontradas", Toast.LENGTH_SHORT).show()
+            }
+
 
         binding.btnDetalles.setOnClickListener{
             val intent = Intent(this, imagenes_unidad::class.java)
--- app/src/main/java/com/example/administradordeunidadesmviles_e2023/estado_unidades.kt
+++ app/src/main/java/com/example/administradordeunidadesmviles_e2023/estado_unidades.kt
@@ -3,39 +3,83 @@ package com.example.administradordeunidadesmviles_e2023
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.administradordeunidadesmviles_e2023.databinding.ActivityEstadoUnidadesBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class estado_unidades : AppCompatActivity() {
     private lateinit var binding:ActivityEstadoUnidadesBinding
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEstadoUnidadesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var UnidadActual=1
+        //Arranca SELECT
+        //
 
-        fun verificador(){
-            binding.anterior.isEnabled = UnidadActual != 1
-            binding.siguiente.isEnabled = UnidadActual != 4
-        }
+        val placasRf = db.collection("automoviles")
+        var nAutosF=0
+        placasRf.get()
+            .addOnSuccessListener { querySnapshot ->
+                val miMapa = hashMapOf<Int, MutableList<String>>()
+                for (document in querySnapshot) {
 
-        binding.siguiente.setOnClickListener{
-            UnidadActual=UnidadActual+1
-            binding.unidadActual.text="Unidades disponibles: "+UnidadActual+"/4"
+                    var datoLeido = document.data
 
-            verificador()
-        }
+                    miMapa.getOrPut(nAutosF) { mutableListOf() }.add(document.id)
 
-        binding.anterior.setOnClickListener {
-            UnidadActual=UnidadActual-1
-            binding.unidadActual.text="Unidades disponibles: "+UnidadActual+"/4"
+                    miMapa.getOrPut(nAutosF) { mutableListOf() }.add((datoLeido?.get("stateConductor") as String).toString())
+                    miMapa.getOrPut(nAutosF) { mutableListOf() }.add((datoLeido?.get("stateUbicacion") as String).toString())
+                    miMapa.getOrPut(nAutosF) { mutableListOf() }.add((datoLeido?.get("stateGasolina") as String).toString())
 
-            verificador()
-        }
+                    nAutosF++
+                }
+
+                binding.unidadActual.text="Unidades disponibles: 1/"+nAutosF
+
+                var UnidadActual=1
+
+                fun verificador(){
+                    binding.anterior.isEnabled = UnidadActual != 1
+                    binding.siguiente.isEnabled = UnidadActual != nAutosF
+                    binding.unidadActual.text="Unidades disponibles: "+UnidadActual+"/"+nAutosF.toString()
+
+
+                    //Codigo que seteara cada cosa en su lugar
+
+                    val valoresUnidadActual=miMapa[UnidadActual-1]
+
+                    binding.txtPlacas.text=valoresUnidadActual?.get(0).toString()
+                    binding.txtChofer.text=valoresUnidadActual?.get(1).toString()
+                    binding.txtUbicacion.text=valoresUnidadActual?.get(2).toString()
+                    binding.txtGasolina.text=valoresUnidadActual?.get(3).toString()
+
+                }
+
+                binding.siguiente.setOnClickListener{
+                    UnidadActual += 1
+                    verificador()
+                }
+
+
+                binding.anterior.setOnClickListener {
+                    UnidadActual -= 1
+                    verificador()
+                }
+
+                verificador()
+
+
+            }
+
 
 
         binding.btnDetalles.setOnClickListener{
             val intent = Intent(this, detalles_avanzados::class.java)
+            intent.putExtra("placas",binding.txtPlacas.text)
             startActivity(intent)
             overridePendingTransition(R.anim.from_right_in, R.anim.from_left_out)
         }
